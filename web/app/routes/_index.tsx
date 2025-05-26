64302c15e172652fa1d7cd1b54eebf84601a8f9f
Ryan Allen <ryanallen@users.noreply.github.com>
2025-05-25 20:36:50 -0700

    Add homepage component with Radix UI components

---
+++ web/app/routes/_index.tsx
@@ -0,0 +1,10 @@
+import { Button, Text, Container } from "@radix-ui/themes";
+
+export default function Index() {
+  return (
+    <Container>
+      <Text size="8">Welcome to Remix + Radix UI!</Text>
+      <Button>Click me</Button>
+    </Container>
+  );
+}
