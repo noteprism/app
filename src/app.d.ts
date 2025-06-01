bc503e20703812301a407cd38f7e5fcdd699f8c9
Ryan Allen <ryanallen@users.noreply.github.com>
2025-06-01 08:50:55 -0700

    code cleanup

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,5 @@
 declare global {
-    namespace App {
-        // interface Error {}
-        // interface Locals {}
-        // interface PageData {}
-        // interface PageState {}
-        // interface Platform {}
-    }
+    namespace App {}
 }
 
 export {}; 
