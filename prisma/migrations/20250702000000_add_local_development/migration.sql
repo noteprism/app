a68ac6b20db3938d9ec07add1167eb0b64cdf9bc
Ryan Allen <ryanallen@users.noreply.github.com>
2025-06-25 20:56:59 -0700

    fix netlify build

--- prisma/migrations/20250702000000_add_local_development/migration.sql
+++ prisma/migrations/20250702000000_add_local_development/migration.sql
@@ -1,12 +1,3 @@
--- Add localDevelopment field to User table if it doesn't exist
-DO $$ 
-BEGIN 
-    IF NOT EXISTS (
-        SELECT 1 
-        FROM information_schema.columns 
-        WHERE table_name = 'User' 
-        AND column_name = 'localDevelopment'
-    ) THEN 
-        ALTER TABLE "User" ADD COLUMN "localDevelopment" BOOLEAN NOT NULL DEFAULT false;
-    END IF;
-END $$; 
+-- This migration is now a no-op since the localDevelopment field was already added in 20250701000000_remove_trial_fields
+-- Keeping this file for migration history tracking purposes
+SELECT 1; 
